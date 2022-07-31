860220c963b9cf8377f3fb2819af536ff2756d66
Mike L <mike.levy.rva@gmail.com>
2022-07-31 19:18:46 -0400

    added node modules and team file links

---
+++ index.js
@@ -0,0 +1,9 @@
+// node modules 
+const inquirer = require('inquirer');
+const fs = require('fs'); 
+
+
+// team files
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
